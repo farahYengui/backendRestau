894fcb14964b6f36983f4c2449521785ddc367a3
farah yengui <farahyengui@gmail.com>
2021-06-23 20:44:34 +0100

    fix

--- controllers/verify.js
+++ controllers/verify.js
@@ -1,9 +1,11 @@
+const mysql = require('../mysql')
+
 function verify(req, res, next) {
-        if (req.user.code === req.body.code) {
+        if (req.user.code === req.body.code && req.user.solde>0) {
                 mysql.query("UPDATE etudiant SET solde=? WHERE cin=?", [req.user.solde - 1, req.user.cin],
                         function (error, results, fields) {
                                 if (error) { return next(error) };
-                                mysql.query("UPDATE liste SET entree=2 WHERE restaurant=?", [req.user.restaurant],
+                                mysql.query("UPDATE liste SET entree=2 WHERE id=?", [req.user.restaurant],
                                         function (error, results, fields) {
                                                 if (error) { return next(error) };
                                                 res.status(200).json({
@@ -13,7 +15,7 @@ function verify(req, res, next) {
                         })
 
         } else {
-                mysql.query("UPDATE liste SET entree=1 WHERE restaurant=?", [req.user.restaurant],
+                mysql.query("UPDATE liste SET entree=1 WHERE id=?", [req.user.restaurant],
                         function (error, results, fields) {
                                 if (error) { return next(error) };
                                 res.status(200).json({
