954e44990b1a2a14174574dcec047364dcd8b54b
farah yengui <farahyengui@gmail.com>
2021-06-27 20:03:53 +0100

    final

--- validation/index.js
+++ validation/index.js
@@ -30,8 +30,8 @@ exports.reclSchema = Joi.object ({
 }).required()
 
 exports.buySchema = Joi.object({
-    type: Joi.any().valid("edinar", "master"),
+    type: Joi.any().valid("Carte e-Dinar", "Carte bancaire"),
     num: Joi.string().length(16).required(),
-    code: Joi.string().length(8).required(),
+    code: Joi.string().required(),
     exp: Joi.string().length(5)
 }).required()
